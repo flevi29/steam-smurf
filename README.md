f5bd5d63d06cded8d27394ed31209fd3a3044bc9
Fodor Levente <levente.f29@gmail.com>
2021-02-14 11:50:55 +0200

    Update demo.gif, update README.md, game is optional now, id selection is made much easier and automatic, optimizations/logging changes

--- README.md
+++ README.md
@@ -1,52 +1,52 @@
 # Steam Smurf
 
-Do you have a _"smurf"_ account? Did you ever feel like
-it's such a pesky task to log off, wait, then copy-paste/remember
-the login info for that account, log in, wait . . 
-*you get the idea*  
+Do you have a _"smurf"_ account? Does the whole process of 
+logging off, waiting, copy-pasting/remembering
+login info, logging in, waiting and so on bother you?  
 
-Well, you don't have to go through that. With one
-simple launch of this script, everything gets handled
-for you.
+You don't have to go through that.
 
-> :warning: Although the chances are low, it is your responsibility if a precious account gets stolen  
+> :warning: Although chances are low, it is your responsibility if a precious account gets stolen  
 > This script [stores your information as plain text](https://www.passcamp.com/blog/dangers-of-storing-and-sharing-passwords-in-plaintext/)
 
 ![demo](demo.gif)
 
 ## How it works
 
-Here's what the script does :
-1. Validates information
+1. Validates provided information, asks for your input if needed
 2. Shuts down currently running Steam process 
-3. Logs in with provided account info
-4. Launches provided title 
-5. Waits for provided title to terminate 
-6. Shuts down Steam 
-7. Launches Steam again so you can log back into 
-   your official/whichever account
+3. Logs in and launches game with provided info  
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_optionally_ ↴
+4. Waits for game to exit _with a hidden window_
+5. Restarts Steam
+
+> :information_source: The optional part only works if the `game` variable is
+> provided, you can find it easily in Task Manager  
+> right click the game's process, and select _Open file location_ 
 
 ## Download & Setup
 
-- [Download smurf.bat](https://github.com/FLevent29/steam-smurf/releases/download/1.1/smurf.bat)
+- [:arrow_double_down:Download smurf.bat](https://github.com/FLevent29/steam-smurf/releases/download/1.1/smurf.bat)
 
-_Information_ the script needs :
+####_Information_ the script needs :
+
 - **Username**
 - **Password**
-- **Steam folder location** (if you did not choose default when installing Steam)
-- **Game's executable location**
-- **Game's Steam ID** (it's right next to `store.steampowered.com/app/` on the [game's store page](https://store.steampowered.com/))
+- **Steam folder path** (if not default)
+- **Game's Steam ID** (:bulb: leave this empty)
+- **Game's executable path** (optional)
 
 > :information_source: All of these can be entered manually, by
-> **editing** the script with a preferred **text editor** like notepad,
-> furthermore it is _**advised**_ to do so, because some wrong 
-> information can only be corrected that way
- 
-On Windows 10 you could set up a _shortcut_ to the 
-script in `%appdata%\Microsoft\Windows\Start Menu\Programs` . .  
-This way you can quickly launch it by pressing 
-<kbd>Win</kbd> and typing in the shortcuts' name,
-also you can set an icon . .
+> **editing** the script with a preferred **text editor** like notepad  
+> - if the `game` variable is wrong, it can only be corrected like so  
+> - if you wish to change anything, it can only be done like so
+> (at the very least by emptying the variables)
+> 
+> :information_source: to find `id` you could just leave it empty,
+> the script finds all the available ones, leaving you to
+> simply select from them . .  
+> Otherwise it's right next to `store.steampowered.com/app/` 
+> on the [game's store page](https://store.steampowered.com/)
 
 Example of how the script should look like for Dota2 ↴
 
@@ -55,7 +55,14 @@ set "username=anonym"
 set "password=mynona"
 
 set "steamFolder=%ProgramFiles(x86)%\Steam"
-set "game=%steamFolder%\steamapps\common\dota 2 beta\game\bin\win64\dota2.exe"
 
 set "id=570"
+
+set "game=%steamFolder%\steamapps\common\dota 2 beta\game\bin\win64\dota2.exe"
 ```
+
+:bulb: On Windows 10 you could set up a _shortcut_ to the
+script in `%appdata%\Microsoft\Windows\Start Menu\Programs` . .  
+This way you can quickly launch it by pressing
+<kbd>Win</kbd> and typing in the shortcuts' name,
+also you can set an icon . .
--- smurf.bat
+++ smurf.bat
@@ -1,25 +1,27 @@
 @echo off
 
 :: Insert your username and password here right after the '=' (equals sign)
-:: NOTE it is your responsibility if a precious account gets stolen this way
-:: as it's written down in plain text
 set "username="
 set "password="
+:: NOTE it is your responsibility if a precious account gets stolen this way
+:: as it's written down in plain text
 
-:: steamFolder is the path to the folder where steam.exe is,
-:: game is the desired game's executable full path
+:: steamFolder is the path to the folder where steam.exe is
 set "steamFolder=%ProgramFiles(x86)%\Steam"
-set "game="
 
 :: id is the game's Steam ID
+set "id="
 :: One easy way to find it is to go to the store page of the game
 :: It's in the address bar right next to 'store.steampowered.com/app/'
-set "id="
 
-:: -!- WARNING You should probably not edit anything from here on WARNING -!-
+:: Optionally game is the desired game's executable full path
+set "game="
+:: Set this up if you wish for Steam to restart when you're done with the game
+:: If you don't know what a path is then write any garbage in there and start the script
+
 ::  ------------------------------------------------------------------------
 
-:: Check PowerShell version 5.1.19042.610 or later
+:: Check if PowerShell version 5.1.19041.610 or later
 powershell -WindowStyle Normal -Command "$versionMinimum = [Version]'5.1.19041.610'; if ($versionMinimum -gt $PSVersionTable.PSVersion) { Write-Host \"This script requires PowerShell $versionMinimum or later\"; exit 1; } exit 0;"
 call :handleError
 
@@ -30,52 +32,70 @@ title %btchtitle%
 set "steam=%steamFolder%\steam.exe"
 set "connectionLogs=%steamFolder%\logs\connection_log.txt"
 
-:: Function or rather string for changing the content
-:: of the script via $content ps variable
-set "editAndKillScript=$cmdpid = (Get-Process | ?{$_.mainWindowTitle -match \"%btchtitle%.*\"} | Select -ExpandProperty Id); $content | Set-Content '%btchtitle%.txt'; Start-Process powershell -WindowStyle Hidden -Wait -ArgumentList \"-Command `\" if ([System.IO.File]::Exists('%btchtitle%.txt')) { Get-Content '%btchtitle%.txt' | Set-Content '%0'; Remove-Item '%btchtitle%.txt' } else { Add-Type -AssemblyName PresentationCore,PresentationFramework; [System.Windows.MessageBox]::Show('Couldn''t create a file necessary to edit the script. Maybe read/write permission in this directory is not suitable, consider moving the script to another directory.', 'Error', 0, 16) | Out-Null; exit 1; } taskkill /PID $cmdpid /F; taskkill /PID $pid /F; `\" \" "
+:: 'Function' for changing the content
+:: of the script via $content PS variable
+set "editAndKillScript=$cmdpid = (Get-Process | ?{$_.mainWindowTitle -match \"%btchtitle%.*\"} | Select -ExpandProperty Id); $content | Set-Content '%btchtitle%.txt'; Start-Process powershell -WindowStyle Hidden -Wait -ArgumentList \"-Command `\" if ([System.IO.File]::Exists('%btchtitle%.txt')) { Get-Content '%btchtitle%.txt' | Set-Content '%0'; Remove-Item '%btchtitle%.txt' } else { Add-Type -AssemblyName PresentationCore,PresentationFramework; [System.Windows.MessageBox]::Show('Couldn''t create a file necessary to edit the script. Maybe read/write permission in this directory is not suitable, consider moving the script to another directory.', 'Error', 0, 16) | Out-Null; exit 1; } taskkill /PID $cmdpid /F; taskkill /PID $pid /F; Start-Process -FilePath '%0'; `\" \" "
+:: Text -less pause for PS
 set "psPause=cmd /c pause | Out-Null"
+:: Press any key to continue
+set "pressAnyCont=Write-Host; Write-Host 'Press any key to continue . .'; %psPause%;"
+:: Folder/file selection 'canceled' state handler function definer
+set "defineSelCanceled=function canceled ($mesg) { Write-Host \"$mesg selection canceled\"; Write-Host \"You can always manually edit this batch file with your preferred text editor, and add what you need that way\"; Write-Host; exit 1; }"
+:: Exit PS if variables are correct with 0, otherwise 1
+set "checkIfVariablesAreGood=if (\"%username%\" -ne '' -and \"%password%\" -ne '' -and \"%steamFolder%\" -ne '' -and !(\"%id%\" -match '\D' -or \"%id%\" -eq '') -and [System.IO.File]::Exists(\"%steam%\") -and (\"%game%\" -eq '' -or ([System.IO.File]::Exists(\"%game%\") -and [System.IO.Path]::GetExtension(\"%game%\") -eq '.exe'))) { exit 0; } exit 1; "
+:: Gets all the game names, IDs and directory names into $gamesname, $gamesid and $gamesdir
+set "getGames=[System.Collections.ArrayList]$gamesname = @(); [System.Collections.ArrayList]$gamesdir = @(); [System.Collections.ArrayList]$gamesid = @(); $acfs = (Get-ChildItem -File -Path '%steamFolder%\steamapps\*.acf').FullName; foreach ($acf in $acfs) { $acfcontent = Get-Content \"$acf\"; foreach ($line in $acfcontent) { $match = [regex]::Match($line, '\"(appid^^^|name^^^|installdir)\"[\s]+\"([\S ]+)\"'); if ($match.Success) { if ($match.Groups[1].Value -eq 'appid') { $gamesid.Add($match.Groups[2].Value) | Out-Null; } if ($match.Groups[1].Value -eq 'name') { $gamesname.Add($match.Groups[2].Value) | Out-Null; } if ($match.Groups[1].Value -eq 'installdir') { $gamesdir.Add($match.Groups[2].Value) | Out-Null; } } } }"
+:: Prints all the games found and selects which game the user wants into $id PS variable
+set "readId=Write-Host 'Select game (type line number)'; for ($j = 0; $j -lt $gamesid.Count; $j++) { Write-Host \"       $($j + 1). $($gamesname[$j]) // $($gamesid[$j]) ($($gamesdir[$j]))\"; } do { $index = (Read-Host \"index\") -as [int]; } while (!($index -ge 1 -and $index -le $gamesname.Count)); $id = $gamesid[$($index - 1)];"
 
 :: Validate variables, add missing/incorrect ones,
-:: at least to whatever extent it's possible
-echo|set /p="Validating variables . . . "
-powershell -Command "if (\"%username%\" -ne \"\" -and \"%password%\" -ne \"\" -and \"%steamFolder%\" -ne \"\" -and \"%game%\" -ne \"\" -and !(\"%id%\" -match '\D' -or \"%id%\" -eq '') -and [System.IO.File]::Exists(\"%steam%\") -and [System.IO.File]::Exists(\"%game%\") -and [System.IO.Path]::GetExtension(\"%game%\") -eq '.exe') { exit 0; } Add-Type -AssemblyName System.Windows.Forms; function anyKeyCont () { Write-Host; Write-Host 'Press any key to continue . .'; %psPause%; } function canceled ($mesg) { Write-Host \"$mesg selection canceled\"; Write-Host \"You can always manually edit this batch file with your preferred text editor, and add what you need that way\"; Write-Host; Write-Host \"Press any key to exit . . .\"; %psPause%; exit 1; } $content = Get-Content \"%0\"; for ($i=0; $i -lt $content.Length; $i++) { if ($content[$i] -match 'set \"username=\"') { $content[$i] = \"set `\"username=$(Read-Host \"Username \")`\"\"; Write-Host; } elseif ($content[$i] -match 'set \"password=\"') { $content[$i] = \"set `\"password=$(Read-Host \"Password \")`\"\"; Write-Host; } elseif ($content[$i] -match 'set \"steamFolder=.*\"' -and ![System.IO.File]::Exists(\"%steam%\")) { Write-Host \"steamFolder variable is incorrect or empty, you're about to set it up\"; anyKeyCont; $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{ ShowNewFolderButton = $false; Description = 'Select Steam folder'; SelectedPath = ${env:ProgramFiles(x86)} }; if ($FolderBrowser.ShowDialog() -eq 'OK') { $content[$i] = \"set `\"steamFolder=$($FolderBrowser.SelectedPath)`\"\"; Write-Host; } else { canceled 'Steam folder'; } } elseif ($content[$i] -match 'set \"game=.*\"' -and (![System.IO.File]::Exists(\"%game%\") -or [System.IO.Path]::GetExtension(\"%game%\") -ne '.exe')) { Write-Host \"game variable is incorrect or empty, you're about to set it up\"; anyKeyCont; $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ Title = 'Select game executable'; InitialDirectory = ${env:ProgramFiles(x86)}; Filter = 'Executable (*.exe)|*.exe' }; if ($FileBrowser.ShowDialog() -eq 'OK') { $content[$i] = \"set `\"game=$($FileBrowser.FileName)`\"\"; Write-Host; } else { canceled 'Game executable'; } } elseif ($content[$i] -match 'set \"id=.*\"' -and (\"%id%\" -match '\D' -or \"%id%\" -eq '')) { Write-Host \"This is the steam id of the selected game, you can easily find it in the game's store page in the address bar\"; $content[$i] = \"set `\"id=$(Read-Host \"Steam game ID \")`\"\"; Write-Host; } } Write-Host \"You'll have to restart this script for it to work with the new information provided . .\"; anyKeyCont; %editAndKillScript%; "
-echo|set /p="OK" & echo.
-
-:: Function, or rather string for exiting Steam
-set "exitSteam=$stmpid = (Get-Process | ?{$_.path -eq \"%steam%\"} | Select -ExpandProperty Id); if ($stmpid -ne $null) { Start-Process -FilePath \"%steam%\" -ArgumentList \"-shutdown\"; Wait-Process -Id $stmpid; } "
-
-:: Attempt login to Steam with provided information
-echo|set /p="Attempting login to Steam . . . "
-powershell -WindowStyle Normal -Command "%exitSteam%; Start-Process -FilePath \"%steam%\" -ArgumentList \"-login %username% %password%\"; exit 0; "
-
-set "setNow=$now = Get-Date -Format 'yyyy-MM-dd HH:mm'"
-set "dateMatch=[regex]::Match($line, \"[\d]{4}-[\d]{2}-[\d]{2} ([\d]{2}:){2}[\d]{2}\")"
-set "invalidPassOrUsername=Write-Host \"Invalid username and password combination\"; Write-Host; $content = Get-Content \"%0\"; $done = 0; for ($i=0; $i -lt $content.Length; $i++) { if ($content[$i] -match 'set \"username=.*\"') { $content[$i] = \"set `\"username=`\"\"; $done++; } elseif ($content[$i] -match 'set \"password=.*\"') { $content[$i] = \"set `\"password=`\"\"; $done++; } if ($done -eq 2) { break; } }; Write-Host \"Press any key to exit . .\"; %psPause%; %editAndKillScript%;"
-
-:: Check if login attempt is successful
-echo|set /p="Checking if login successful . . . "
-powershell -WindowStyle Normal -Command "%setNow%; $retries = 0; while ($retries -ge 0) { if ($retries -ge 50) { Write-Host \"`r`nSomething went wrong, maybe Steam got stuck . . .\"; exit 1; } $content = Get-Content \"%connectionLogs%\" | select -Last 50; foreach ($line in $content) { $match = %dateMatch%; if ($match.Success -and $match.Value -ge $now) { $match = [regex]::Match($line, \"WGToken^|Connection Failed^|Invalid Password\"); if ($match.Success) { $retries = -2; break; } } } $retries++; Start-Sleep -s 1; } switch ($match.Value) { \"Invalid Password\" { %invalidPassOrUsername%; } \"Connection Failed\" { Write-Host \"`r`nSomething went wrong. Maybe there's no internet connection . . .`r`n\"; exit 1; } \"WGToken\" { Start-Sleep -s 1; exit 0; } } exit 1; "
+:: at least to whatever extent it's reasonable
+echo|set /p="Checking variables . . . "
+powershell -Command "%checkIfVariablesAreGood%"
+if %errorlevel% equ 0 (goto :Valid)
+echo|set /p="Invalid" & echo.
+powershell -Command "Add-Type -AssemblyName System.Windows.Forms; %defineSelCanceled%; $content = Get-Content '%0'; for ($i=0; $i -lt $content.Length; $i++) { if ($content[$i] -match 'set \"username=\"') { $content[$i] = \"set `\"username=$(Read-Host \"Username \")`\"\"; } elseif ($content[$i] -match 'set \"password=\"') { $content[$i] = \"set `\"password=$(Read-Host \"Password \")`\"\"; } elseif ($content[$i] -match 'set \"steamFolder=.*\"' -and ![System.IO.File]::Exists(\"%steam%\")) { Write-Host \"`r`nsteamFolder variable is incorrect or empty, you're about to set it up\"; %pressAnyCont%; $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{ ShowNewFolderButton = $false; Description = 'Select Steam folder'; SelectedPath = ${env:ProgramFiles(x86)} }; if ($FolderBrowser.ShowDialog() -eq 'OK') { $content[$i] = \"set `\"steamFolder=$($FolderBrowser.SelectedPath)`\"\"; } else { canceled 'Steam folder'; } } elseif ($content[$i] -match 'set \"game=.+\"' -and (![System.IO.File]::Exists(\"%game%\") -or [System.IO.Path]::GetExtension(\"%game%\") -ne '.exe')) { Write-Host \"`r`ngame variable is incorrect, you're about to set it up\"; %pressAnyCont%; $common = \"${env:ProgramFiles(x86)}\Steam\steamapps\common\"; $iniDir = if (Test-Path $common) { $common } else { ${env:ProgramFiles(x86)} }; $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ Title = 'Select game executable'; InitialDirectory = $iniDir; Filter = 'Executable (*.exe)|*.exe' }; if ($FileBrowser.ShowDialog() -eq 'OK') { $content[$i] = \"set `\"game=$($FileBrowser.FileName)`\"\"; } else { canceled 'Game executable'; } } elseif ($content[$i] -match 'set \"id=.*\"' -and (\"%id%\" -match '\D' -or \"%id%\" -eq '')) { Write-Host \"Game ID wrong or empty`r`n\"; %getGames%; %readId%; $content[$i] = \"set `\"id=$id`\"\"; } } Write-Host 'Press any key to write new information to script . .'; %psPause%; %editAndKillScript%; "
 call :handleError
+:Valid
 echo|set /p="OK" & echo.
 
-:: Starts game specified by id
-set "startGame=Start-Process -FilePath \"%steam%\" -ArgumentList \"-applaunch %id%\""
+:: Function for terminating current Steam process
+set "exitSteam=$stmpid = (Get-Process | ?{$_.path -eq \"%steam%\"} | Select -ExpandProperty Id); if ($stmpid -ne $null) { & '%steam%' -shutdown; Wait-Process -Id $stmpid; } "
+:: Saves current time and date into $now PS variable
+set "setNow=$now = Get-Date -Format 'yyyy-MM-dd HH:mm'"
+:: PS regex match string for the date format above
+set "dateMatch=[regex]::Match($line, \"[\d]{4}-[\d]{2}-[\d]{2} ([\d]{2}:){2}[\d]{2}\")"
+:: PS handler for invalid pass/username, rewrites script emptying username and password variables
+set "invalidPassOrUsername=Write-Host \"Failed`r`nInvalid username and password combination\"; Write-Host; $content = Get-Content \"%0\"; $done = 0; for ($i=0; $i -lt $content.Length; $i++) { if ($content[$i] -match 'set \"username=.*\"') { $content[$i] = \"set `\"username=`\"\"; $done++; } elseif ($content[$i] -match 'set \"password=.*\"') { $content[$i] = \"set `\"password=`\"\"; $done++; } if ($done -eq 2) { break; } }; Write-Host \"Press any key to exit . .\"; %psPause%; %editAndKillScript%;"
+:: Decides what to do depending on the value of $match PS variable
+set "switchMatch=switch ($match.Value) { 'Invalid Password' { %exitSteam%; %invalidPassOrUsername%; } 'Connection Failed' { Write-Host \"Failed`r`nSomething went wrong. Maybe there's no internet connection . . .`r`n\"; %exitSteam%; exit 1; } 'WGToken' { exit 0; } }"
+
+:: Kill running Steam and attempt login to Steam with provided information
+:: Then check if login attempt is successful
+echo|set /p="Attempting login to Steam and launching game . . . "
+powershell -WindowStyle Normal -Command "%exitSteam%; & '%steam%' -silent -login %username% %password% -applaunch %id%; %setNow%; $retries = 0; while ($retries -ge 0) { if ($retries -ge 50) { Write-Host \"`r`nSomething went wrong, maybe Steam got stuck or didn't start . . .\"; exit 1; } $content = Get-Content \"%connectionLogs%\" | select -Last 50; foreach ($line in $content) { $match = %dateMatch%; if ($match.Success -and $match.Value -ge $now) { $match = [regex]::Match($line, \"WGToken^|Connection Failed^|Invalid Password\"); if ($match.Success) { $retries = -2; break; } } } $retries++; Start-Sleep -s 1; } %switchMatch%; exit 1; "
+call :handleError
+echo|set /p="login OK" & echo.
+
+:: If game variable isn't defined, then we're done
+if "%game%" == "" (exit 0)
+
 :: Alerts user of failed game startup with a message box
 set "alertUserOfFailure=Add-Type -AssemblyName PresentationCore,PresentationFramework; [System.Windows.MessageBox]::Show(\"No running process of the game detected. Maybe it's updating/installing, it crashed or something else.`r`nOr maybe ID and selected game executable don't match/are wrong (in this case you'll have to manually edit this script).\", 'Problem detecting or launching Steam title', 0, 48) | Out-Null"
 :: Gets game PID from executable
 set "getGamePid=(Get-Process | ?{$_.path -eq \"%game%\"} | Select -ExpandProperty Id)"
 
-:: Try starting and detecting the game
-echo|set /p="Attempting to start and detect game . . . "
-powershell -WindowStyle Normal -Command "%startGame%; $retries = 0; do { Start-Sleep -s 1; $gmpid = %getGamePid%; $retries++; } while ($gmpid -eq $null -and $retries -lt 5); if ($gmpid -eq $null) { %alertUserOfFailure%; exit 1; } exit 0; "
-if %errorlevel% equ 0 (goto :StartGame)
+:: Try detecting the game
+echo|set /p="Detecting game . . . "
+powershell -WindowStyle Normal -Command "$retries = 0; do { Start-Sleep -s 1; $gmpid = %getGamePid%; $retries++; } while ($gmpid -eq $null -and $retries -lt 5); if ($gmpid -eq $null) { %alertUserOfFailure%; exit 1; } exit 0; "
+if %errorlevel% equ 0 (goto :GameStarted)
 echo|set /p="Failed" & echo.
 call :handleError
 
 :: Wait for game to exit with a hidden console window
-:StartGame
+:GameStarted
 echo|set /p="Success" & echo. & echo|set /p="Waiting for game to exit . . . "
-powershell -WindowStyle Hidden -Command "Wait-Process -Id %getGamePid%; %exitSteam%; Start-Process -FilePath \"%steam%\"; exit 0; "
+powershell -WindowStyle Hidden -Command "Wait-Process -Id %getGamePid%; %exitSteam%; & '%steam%'; exit 0; "
 
 exit 0
 
